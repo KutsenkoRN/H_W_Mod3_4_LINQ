98acf599183213ebd0a1895ed694190f5695e4af
Roman Kutsenko <73640616+KutsenkoRN@users.noreply.github.com>
2023-08-14 21:57:33 +0300

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_W_Mod3_4
+{
+    internal class User
+    {
+        public User()
+        {
+            Name = string.Empty;
+            PhoneNumber = string.Empty;
+        }
+
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public override string ToString()
+        {
+            return Name + " " + PhoneNumber;
+        }
+    }
+}
+
